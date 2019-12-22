e752e3fa294344fccad71c937eecf86afc91059e
haxul <s.s.starodubov@yandex.ru>
2019-12-22 14:16:32 +0400

    fix curry

--- curry.js
+++ curry.js
@@ -1,12 +1,12 @@
-const { curry } = require("ramda")
+const { curry, flip, tail } = require("ramda")
 const add = (a, y) => a + y
 const devide = (x, y) => x / y
 
 const toPair = f => (x, y) => f(x, y)
 const result = toPair(add)(1, 2)
 
-const flip = f => (x, y) => f(y, x)
-const result2 = flip(devide)(2, 1)
+// const flip = f => (x, y) => f(y, x)
+// const result2 = flip(devide)(2, 1)
 
 // const curry = f => a => b => f(a, b)
 const result3 = curry(add)(2)(2)
@@ -33,3 +33,6 @@ const res = splitterByEq("hello=world")
 const miner = curry((a, b) => b - a)
 const miner10 = miner(10)
 const r = miner10(100)
+
+const devider = (x, y) => x / y
+const result7 = flip(devider)(10, 5)
